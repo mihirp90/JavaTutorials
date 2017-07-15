c8dd3bd0fca9c0530955ee27c7f5b4914966bd35
Mihir <mihirp90@gmail.com>
2017-07-15 13:09:33 +0530

    new version1

---
+++ src/com/java/core/basic/DataTypes.java
@@ -0,0 +1,25 @@
+package com.java.core.basic;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int intVar;
+		float floatVar;
+		double doubleVar;
+		char charVar;
+		short shortVar;
+		long longVar;*/
+		
+		System.out.println("MAX_VALUE"+ "                      " + "MIN_VALUE");
+		System.out.println("_________________________________________________");
+		System.out.println(Integer.MAX_VALUE+"                 " + Integer.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE+"                   " + Float.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE+"                  " + Double.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE+"                   " + Short.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE+"                    " + Long.MIN_VALUE);
+		System.out.println(Character.MAX_VALUE+"               " + Character.MIN_VALUE);
+		
+	}
+
+}
