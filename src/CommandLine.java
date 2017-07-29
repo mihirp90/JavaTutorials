c98ccda959d4373698b21748392a6151d49385f1
Mihir <mihirp90@gmail.com>
2017-07-29 23:13:12 +0530

    new version of java  file

---
+++ src/CommandLine.java
@@ -0,0 +1,14 @@
+
+public class CommandLine {
+
+	public static void main(String[] s) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(s[1]);
+		System.out.println(s[2]);
+		System.out.println(s[3]);
+		System.out.println("Commandd Line Arguments Example");
+
+	}
+
+}
